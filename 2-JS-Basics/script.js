88baf61c7b22243875e2862dea9eee77efbed7ba
Jurek <jurek2006@gmail.com>
2017-09-03 17:05:25 +0200

    Pobrano pliki do sekcji 3

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,26 +1,16 @@
-const player1Height = 151;
-const player1Age = 33;
-const player2Height = 170;
-const player2Age = 33;
-const player3Height = 170;
-const player3Age = 33;
+const born1 = [1965, 2008, 1992];
+const born2 = [1923, 2011, 2000, 1999];
 
-let score1 = player1Height + 5 * player1Age;
-let score2 = player2Height + 5 * player2Age;
-let score3 = player3Height + 5 * player3Age;
-
-if( score1 > score2 && score1 > score3 ){
-	console.log("Wygrał Player1 z wynikiem " + score1 + ".");
-} else if (score1 < score2 && score2 > score3){
-	console.log("Wygrał Player2 z wynikiem " + score2 + ".");
-} else if (score1 < score3 && score2 < score3){
-	console.log("Wygrał Player3 z wynikiem " + score3 + ".");
-} else if(score1 === score2 && score1 > score3){
-	console.log("Wygrali ex-aequo Player1 i Player2 z wynikiem " + score1 + ".");
-} else if(score1 === score3 && score1 > score2){
-	console.log("Wygrali ex-aequo Player1 i Player3 z wynikiem " + score1 + ".");
-} else if(score2 === score3 && score2 > score1){
-	console.log("Wygrali ex-aequo Player2 i Player3 z wynikiem " + score2 + ".");
-} else {
-	console.log("Remis. Wszyscy gracze mieli " + score1 + " punktów.");
+function printFullAge(born){
+	const fullAge = new Array();
+	for(let i = 0; i < born.length; i++){
+		fullAge.push(2017 - born[i] >= 18 ? true : false );
+	}
+	return fullAge;
 }
+
+let fullAge1 = printFullAge(born1);
+let fullAge2 = printFullAge(born2);
+
+console.log(fullAge1);
+console.log(fullAge2);
