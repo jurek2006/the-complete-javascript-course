cce16dff0eead093524580e0e8ea64e9e7df05e1
jurek2006 <jurek2006@gmail.com>
2017-09-07 11:39:38 +0200

    Rozwiazanie coding challenge4 z rozwiazaniem Jonasa (closures)

--- 5-Advanced-JS/5-Advanced-JS/challenge.js
+++ 5-Advanced-JS/5-Advanced-JS/challenge.js
@@ -17,17 +17,29 @@
 		}
 	}
 
-	Question.prototype.checkUserAnswer = function(userAnswer){
+	Question.prototype.checkUserAnswer = function(userAnswer, callback){
+		var sc;
+
 		if(userAnswer === this.corrAnswInd){
 			console.log("Odpowiedź prawidłowa.");
-			return true;
+			// return true; //z mojego rozwiązania
+			sc = callback(true); //z rozwiązania Jonasa
 		} else if (userAnswer > 0 && userAnswer < this.answers.length){
 			console.log("Odpowiedź nieprawidłowa.");
-			return false;
+			// return false; //z mojego rozwiązania
+			sc = callback(false); //z rozwiązania Jonasa
 		} else {
 			console.log("Brak odpowiedzi o numerze " + userAnswer + " do podanego pytania.");
-			return false;
+			// return false; //z mojego rozwiązania
+			sc = callback(false); //z rozwiązania Jonasa
 		}
+
+		this.displayScore(sc);
+	}
+
+	Question.prototype.displayScore = function(score){
+		console.log("Wynik: " + score);
+		console.log(' -------------------');
 	}
 
 	// Create a couple of questions using the constructor and store them inside the array
@@ -50,25 +62,54 @@
 						['1911', '1914', '1945', '1939'],
 						3
 					));
-	// SPOSÓB WHILE
-	var score = 0;
-	while(true){
+	// // SPOSÓB WHILE - mój
+	// var score = 0;
+	// while(true){
+
+	// 	// Select one random question and log it on the console, with possible answers
+	// 	var questionNum = Math.floor(Math.random() * questions.length);
+	// 	questions[questionNum].consoleQuestion();
+
+	// 	// Use the 'prompt' to ask user for the correct answer
+	// 	// If user input 'exit' - finish the loop
+	// 	userAnswerNum = prompt("Podaj numer prawidłowej odpowiedzi");
+	// 	if(userAnswerNum === 'exit'){ break }
+	// 	else {userAnswerNum = parseInt(userAnswerNum)}
+	// 	if(questions[questionNum].checkUserAnswer(userAnswerNum)){
+	// 	// if function returns true === correct answer
+	// 		score++;
+	// 	}
+	// 	console.log('Wynik: ' + score);
+	// }
+
+	// SPOSÓB Jonasa
+
+	function score(){
+		var sc = 0;
+		return function(correct){
+			if(correct) { sc++ }
+			return sc;
+		}
+	}
+
+	var keepScore = score();
+
+	function nextQuestion(){
 
 		// Select one random question and log it on the console, with possible answers
 		var questionNum = Math.floor(Math.random() * questions.length);
 		questions[questionNum].consoleQuestion();
 
-		// Use the 'prompt' to ask user for the correct answer
-		// If user input 'exit' - finish the loop
 		userAnswerNum = prompt("Podaj numer prawidłowej odpowiedzi");
-		if(userAnswerNum === 'exit'){ break }
-		else {userAnswerNum = parseInt(userAnswerNum)}
-		if(questions[questionNum].checkUserAnswer(userAnswerNum)){
-		// if function returns true === correct answer
-			score++;
+
+		if (userAnswerNum !== 'exit'){
+			questions[questionNum].checkUserAnswer(parseInt(userAnswerNum), keepScore)
+			nextQuestion();
 		}
-		console.log('Wynik: ' + score);
+
 	}
+
+	nextQuestion();
 
 
 })();
