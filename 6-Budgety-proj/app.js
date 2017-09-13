98e8ad42e9e21dd32548a18eb68a11efd3dc2960
Jurek <jurek2006@gmail.com>
2017-09-13 21:50:40 +0200

    Po wykladzie 78 - dodano wyswietlanie budżetu, sumy wpływów i wydatków w UI - zakończenie etepu I budowy aplikacji

--- 6-Budgety-proj/app.js
+++ 6-Budgety-proj/app.js
@@ -102,7 +102,11 @@ var UIController = (function(){
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	}
 
 	return {
@@ -152,6 +156,14 @@ var UIController = (function(){
 			fieldsArr[0].focus();
 		},
 
+		displayBudget: function(obj){
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			document.querySelector(DOMstrings.percentageLabel).textContent = (obj.percentage > 0) ? obj.percentage+'%' : '---';
+		},
+
 		getDOMstrings: function(){
 			return DOMstrings;
 		}
@@ -183,7 +195,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-		console.log(budget);
+		UIController.displayBudget(budget);
 	}
 
 	var ctrlAddItem = function(){
@@ -211,6 +223,12 @@ var controller = (function(budgetCtrl, UICtrl){
 	return {
 		init: function(){
 			console.log('Application has started');
+			UIController.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListeners();
 		}
 	};
