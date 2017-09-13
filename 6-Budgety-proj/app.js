e2f6b87aef88577263b1a1e0726826d7e41f93b2
Jurek <jurek2006@gmail.com>
2017-09-13 19:58:01 +0200

    Po wykładzie 76 - konwersja string na float, zapobieganie akcji, jesli nie sa wypełnione pola formularza

--- 6-Budgety-proj/app.js
+++ 6-Budgety-proj/app.js
@@ -77,7 +77,7 @@ var UIController = (function(){
 			return {
 				type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			}
 		},
 
@@ -142,25 +142,34 @@ var controller = (function(budgetCtrl, UICtrl){
 		});
 	};
 
+	var updateBudget = function(){
+		// 1. Calculate the budget
+
+		// 2. Return the budget
+
+		// 3. Display the budget on the UI
+	}
+
 	var ctrlAddItem = function(){
 		var input, newItem;
 
 		// 1. Get the field input data
 		input = UIController.getinput();
 
-		// 2. Add the item to the budget controller
-		newItem = budgetController.addItem(input.type, input.description, input.value);
+		if(input.description !== "" && !isNaN(input.value) && input.value > 0){
 
-		// 3. Add the item to the UI
-		UIController.addListItem(newItem, input.type);
+			// 2. Add the item to the budget controller
+			newItem = budgetController.addItem(input.type, input.description, input.value);
 
-		// 4. Clear the fields
-		UIController.clearFields();
+			// 3. Add the item to the UI
+			UIController.addListItem(newItem, input.type);
 
-		// 4. Calculate the budget
+			// 4. Clear the fields
+			UIController.clearFields();
 
-		// 5. Display the budget on the UI
-
+			// 5. Calculate and update budget
+			updateBudget();
+		}
 	}
 
 	return {
