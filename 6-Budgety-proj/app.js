2317ad8a955bc5a8a2658c607dabd443ae39dc15
Jurek <jurek2006@gmail.com>
2017-09-17 21:09:19 +0200

    Po wykł 89 - wyświetlanie aktualnego miesiąca i roku

--- 6-Budgety-proj/app.js
+++ 6-Budgety-proj/app.js
@@ -158,7 +158,8 @@ var UIController = (function(){
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	}
 
 	var formatNumber = function(num, type){
@@ -262,6 +263,16 @@ var UIController = (function(){
 					current.textContent = '---';
 				}
 			});
+		},
+
+		displayMonth: function () {
+			var now, year, month, months;
+			
+			now = new Date();
+			var formatter = new Intl.DateTimeFormat("pl", { month: "long" });
+			month = formatter.format(now);
+			year = now.getFullYear();
+			document.querySelector(DOMstrings.dateLabel).textContent = month + ' ' + year;
 		},
 
 		getDOMstrings: function(){
@@ -372,6 +383,7 @@ var controller = (function(budgetCtrl, UICtrl){
 	return {
 		init: function(){
 			console.log('Application has started');
+			UIController.displayMonth();
 			UIController.displayBudget({
 				budget: 0,
 				totalInc: 0,
