0dfd59ab167504f5759bdf7fb1a6d6d214ed17f3
Jurek <jurek2006@gmail.com>
2017-09-17 18:47:45 +0200

    Zakończenie wykładu 86 - obliczanie w budgetConroller procentów dla każdego z wydatków.

--- 6-Budgety-proj/app.js
+++ 6-Budgety-proj/app.js
@@ -5,6 +5,20 @@ var budgetController = (function(){
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
+	};
+	Expense.prototype.calcPercentage = function(){
+		var totalIncome = data.totals.inc;
+
+		if(totalIncome > 0) {
+			this.percentage = Math.round(this.value / totalIncome * 100);
+		} else {
+			this.percentage = -1; 
+		}
+	};
+
+	Expense.prototype.getPercentage = function(){
+		return this.percentage;
 	};
 
 	var Income = function(id, description, value){
@@ -96,6 +110,20 @@ var budgetController = (function(){
 			} else {
 				data.percentage = -1;
 			}
+		},
+
+		calculatePercentages: function(){
+
+			data.allItems.exp.forEach(function(cur){
+				cur.calcPercentage();
+			});
+		},
+
+		getPercentages: function(){
+			var allPerc = data.allItems.exp.map(function(cur){
+				return cur.getPercentage();
+			});
+			return allPerc;
 		},
 
 		getBudget: function(){
@@ -228,6 +256,18 @@ var controller = (function(budgetCtrl, UICtrl){
 		UIController.displayBudget(budget);
 	}
 
+	var updatePercentages = function(){
+		
+		// 1. Calculate percentages
+		budgetCtrl.calculatePercentages();
+
+		// 2. Read percentages from the budget controller
+		var percentages = budgetCtrl.getPercentages();
+
+		// 3. Update the UI with the new percentages
+		console.log(percentages);
+	}
+
 	var ctrlAddItem = function(){
 
 		// 1. Get the field input data
@@ -246,6 +286,9 @@ var controller = (function(budgetCtrl, UICtrl){
 
 			// 5. Calculate and update budget
 			updateBudget();
+
+			// 6. Calculate and update percentages
+			updatePercentages();
 		}
 	}
 
@@ -276,6 +319,9 @@ var controller = (function(budgetCtrl, UICtrl){
 
 			// 3. Update and show the new budget
 			updateBudget();
+
+			// 4. Calculate and update percentages
+			updatePercentages();
 		}
 	}
 
