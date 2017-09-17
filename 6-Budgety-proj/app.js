e014026a8f3488013bdd19812118605dd06056a8
Jurek <jurek2006@gmail.com>
2017-09-17 19:51:37 +0200

    Po wykładzie 87 - zakończenie funkcjonalności procentów przy wydatkach (wyświetlanie ich przy każdym z nich w UI)

--- 6-Budgety-proj/app.js
+++ 6-Budgety-proj/app.js
@@ -157,7 +157,8 @@ var UIController = (function(){
 		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
-		container: '.container'
+		container: '.container',
+		expensesPercLabel: '.item__percentage'
 	}
 
 	return {
@@ -220,6 +221,26 @@ var UIController = (function(){
 			document.querySelector(DOMstrings.percentageLabel).textContent = (obj.percentage > 0) ? obj.percentage+'%' : '---';
 		},
 
+		displayPercentages: function(percentages){
+
+			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+			var nodeListForEach = function (list, callback) {
+				for(var i = 0; i < list.length; i++){
+					callback(list[i], i);
+				}
+			}
+
+			nodeListForEach(fields, function(current, index){
+
+				if(percentages[index] > 0){
+					current.textContent = percentages[index] + '%';
+				} else {
+					current.textContent = '---';
+				}
+			});
+		},
+
 		getDOMstrings: function(){
 			return DOMstrings;
 		}
@@ -265,7 +286,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		var percentages = budgetCtrl.getPercentages();
 
 		// 3. Update the UI with the new percentages
-		console.log(percentages);
+		UICtrl.displayPercentages(percentages);
 	}
 
 	var ctrlAddItem = function(){
